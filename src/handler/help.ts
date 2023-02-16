eb8ed95bceec5fb269b43e1d51bd02b880111d14
bincooo <bingco.zn@gmail.com>
2023-02-16 16:23:52 +0800

    新增群成员发送消息

--- src/handler/help.ts
+++ src/handler/help.ts
@@ -4,7 +4,11 @@ import { buildHelpMessage } from 'src/command/commands/help'
 export const helpHandler: MessageHandler = function (sender) {
   const e: MessageEvent = sender.getEventObject()
   if (e.notice_type === 'group' && e.sub_type === 'increase') {
-    e.group.sendMsg('欢迎新人加入, @小爱Ai+[文本] 即可与Ai对话哦~\n\n' + buildHelpMessage(false), true)
+    try {
+      e.group.sendMsg('欢迎'+e.nickname+'加入, @小爱Ai+[文本] 即可与Ai对话哦~\n\n' + buildHelpMessage(false), false)
+    } catch(err) {
+      console.log('helpHandler err: ', err)
+    }
     return false
   }
   return true
