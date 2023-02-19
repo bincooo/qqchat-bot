fa8e953723cc97b195b2f3cff24972e2effe9d2d
bincooo <bingco.zn@gmail.com>
2023-02-19 19:06:20 +0800

    新的图片画质提升工具

---
+++ src/util/browser.ts
@@ -0,0 +1,79 @@
+import type { Browser, Page } from 'puppeteer'
+import puppeteer from 'puppeteer-extra'
+import fs from 'fs'
+import os from 'os'
+
+
+let DEFAULT_BROWSER: null | Browser = null
+export default async function getBrowser(): (Browser | Page)[] {
+  if (DEFAULT_BROWSER) return [DEFAULT_BROWSER, null]
+
+  const puppeteerArgs = [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-infobars',
+    '--disable-dev-shm-usage',
+    '--disable-blink-features=AutomationControlled',
+    '--ignore-certificate-errors',
+    '--no-first-run',
+    '--no-service-autorun',
+    '--password-store=basic',
+    '--system-developer-mode',
+    // the following flags all try to reduce memory
+    // '--single-process',
+    '--mute-audio',
+    '--disable-default-apps',
+    '--no-zygote',
+    '--disable-accelerated-2d-canvas',
+    '--disable-web-security'
+    // '--disable-gpu'
+    // '--js-flags="--max-old-space-size=1024"'
+  ]
+
+
+  const browser: Browser = await puppeteer.launch({
+    headless: false,
+    // devtools: true,
+    args: puppeteerArgs,
+    ignoreDefaultArgs: [
+      '--disable-extensions',
+      '--enable-automation',
+      '--disable-component-extensions-with-background-pages'
+    ],
+    ignoreHTTPSErrors: true,
+    executablePath: defaultChromeExecutablePath()
+  })
+
+  const [ page ] = await browser.pages()
+  DEFAULT_BROWSER = browser
+  return [browser, page]
+}
+
+
+export const defaultChromeExecutablePath = (): string => {
+  // return executablePath()
+
+  if (process.env.PUPPETEER_EXECUTABLE_PATH) {
+    return process.env.PUPPETEER_EXECUTABLE_PATH
+  }
+
+  switch (os.platform()) {
+    case 'win32':
+      return 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+
+    case 'darwin':
+      return '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+    default: {
+      /**
+       * Since two (2) separate chrome releases exist on linux, we first do a
+       * check to ensure we're executing the right one.
+       */
+      const chromeExists = fs.existsSync('/usr/bin/google-chrome')
+
+      return chromeExists
+        ? '/usr/bin/google-chrome'
+        : '/usr/bin/google-chrome-stable'
+    }
+  }
+}
