1ca439fd393107eb5b86b2ec019bd3e65df2e4a2
bincooo <bingco.zn@gmail.com>
2023-02-26 18:53:56 +0800

    代码篇幅过长问题

--- src/filter/code.ts
+++ src/filter/code.ts
@@ -24,8 +24,8 @@ export class CodeFilter extends BaseMessageFilter {
 
     if (content.startsWith('[md]') || content.startsWith('[md:latex]')) {
       resultMessage = [
-          (content.startsWith('[md:latex]') ? 'latex格式\n' : ''),
-          '用我定义的格式回复，格式为```markdown [xx] ```，[xx]替换成你的回答：\n\n',
+          (content.startsWith('[md:latex]') ? 'latex格式' : ''),
+          '用我定义的格式回复，格式为```markdown [xx] ```，[xx]替换成你的回答，理解以上内容后回答我的问题:\n\n',
           content.replace('[md]', '')
             .replace('[md:latex]', '')
         ]
