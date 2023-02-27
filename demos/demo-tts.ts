ce88448e46cb21adeaa085a54b66f336da52ab0a
bincooo <bingco.zn@gmail.com>
2023-02-28 03:30:07 +0800

    日文识别错误修复

--- demos/demo-tts.ts
+++ demos/demo-tts.ts
@@ -4,16 +4,18 @@ import fs from 'fs'
 
 async function main() {
   const text = 'こんにちはご主人様'
+  // const text = '你好呀我的主人'
   const count = japaneseUnicodeParser.count(text)
   const isJapan = () => {
     return text.length / 2 < count
   }
+  console.log('isJapan', isJapan())
 
-  const path = await speak({
-    text,
-    vname: isJapan() ? 'ja-JP-AoiNeural' : null
-  }, 'audio-24khz-96kbitrate-mono-mp3')
-  console.log('转化成功: ' + path)
+  // const path = await speak({
+  //   text,
+  //   vname: isJapan() ? 'ja-JP-AoiNeural' : null
+  // }, 'audio-24khz-96kbitrate-mono-mp3')
+  // console.log('转化成功: ' + path)
 }
 
 main()
--- src/util/lang.ts
+++ src/util/lang.ts
@@ -2,9 +2,10 @@
 
 class JapaneseUnicodeParser {
   protected _interval: Array<{ l: number, r: number }> = [
+      { l: 0x3000, r: 0x303f },
       { l: 0x3040, r: 0x309F },
-      { l: 0x30A0, r: 0x30FF },
-      { l: 0x4E00, r: 0x9FBF }
+      { l: 0x30A0, r: 0x30FF }
+      // { l: 0x4E00, r: 0x9FBF }
     ]
 
   assert(code: number): boolean {
