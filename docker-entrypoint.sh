f4cd59823958a77b1702889eb42dc8929a9b4c9b
bincooo <bingco.zn@gmail.com>
2023-03-02 01:06:39 +0800

    关于pc语音播放的一些尝试

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-PID=`ps -ef|grep Xvfb|grep -v grep|awk '{print $2}'`
-echo "Xvfb pid =$PID"
-if [ -n "$PID" ]; then
+
+
+# if [ -n "$PID" ]; then
+#   export DISPLAY=:99
+# else
+#   Xvfb :99 -ac & export DISPLAY=:99
+# fi
+
+{
+  Xvfb :99 -ac && export DISPLAY=:99 &&
+} || {
   export DISPLAY=:99
-else
-  Xvfb :99 -ac & export DISPLAY=:99
-fi
+}
 
 if [ $ENABLED_X11VNC == 'yes' ]; then
   x11vnc -display :99 -forever -bg -o /var/log/x11vnc.log -rfbport 5900
