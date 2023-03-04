4089814f2a9698fda98133c2cdfe6bd1c5290170
bincooo <bingco.zn@gmail.com>
2023-03-04 17:09:12 +0800

    语法错误

--- src/filter/default.ts
+++ src/filter/default.ts
@@ -25,12 +25,12 @@ function dat() {
   const ts = new Date()
     .getTime()
   const date = new Date(ts + 1000 * 60 * 60 * 8)
-  const y = inst.getFullYear()
-  const m = inst.getMonth()+1
-  const d = inst.getDate()
-  const h = inst.getHours()+1
-  const mm = inst.getMinutes()
-  const s = inst.getSeconds()
+  const y = date.getFullYear()
+  const m = date.getMonth()+1
+  const d = date.getDate()
+  const h = date.getHours()+1
+  const mm = date.getMinutes()
+  const s = date.getSeconds()
   const fmt = (n: number) => {
     return (n < 10 ? '0' + n : n)
   }
