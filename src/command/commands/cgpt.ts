3238e021839f5e8ef62ef49502aaa2e36937294f
bincooo <bingco.zn@gmail.com>
2023-03-05 23:10:34 +0800

    tag域名修改

--- src/command/commands/cgpt.ts
+++ src/command/commands/cgpt.ts
@@ -14,7 +14,7 @@ class CgptCommand extends BaseCommand {
     + '\n!reset  - 重置会话'
     + '\n/draw [tag] - ai作画\n'
     + '\n[tag]+文字 - 让Ai根据文字生成tag'
-    + '\nhttp://h.icu-web.tk:8082/tag\n可通过该站点定制ai绘画标签'
+    + '\nhttps://www.icu-web.tk:8082/tag\n可通过该站点定制ai绘画标签'
     + '\n----'
     + '\n[code]+文字 - 代码片段优化(代码片段转图片)'
     + '\n[md(:latex)]+文字 - [md]以md格式转图片,[md:latex]以md格式转图片(数学公式增强)'
