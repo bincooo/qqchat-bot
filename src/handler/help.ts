294aac838cc72646395250a7327aa3dc97dcd69a
bincooo <bingco.zn@gmail.com>
2023-03-05 22:38:44 +0800

    欢迎余修改

--- src/handler/help.ts
+++ src/handler/help.ts
@@ -5,17 +5,7 @@ import { getClient } from 'src/core/oicq'
 export const helpHandler: MessageHandler = function (sender) {
   const e: MessageEvent = sender.getEventObject()
   if (e.notice_type === 'group' && e.sub_type === 'increase') {
-    try {
-      e.group.sendMsg('欢迎🌹🌹' + e.nickname + '🎉🎉加入\n@' + config.botNickname + ' /help 有惊喜哦 ~ ✨', false)
-        .then(result => {
-          setTimeout(
-            () => getClient()?.deleteMsg(result.message_id),
-            5000
-          )
-        })
-    } catch(err) {
-      console.log('HelpHandler:Error', err)
-    }
+    e.group.sendMsg('欢迎🌹🌹' + e.nickname + '🎉🎉加入\n@' + config.botNickname + ' /help 有惊喜哦 ~ ✨', false)
     return false
   }
   return true
