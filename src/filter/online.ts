26b62c3331da7ed1f3c854ad46cf2e35da4775ae
bincooo <bingco.zn@gmail.com>
2023-03-05 19:27:38 +0800

    在线模式优化

--- src/filter/online.ts
+++ src/filter/online.ts
@@ -25,8 +25,9 @@ export class OnlineFilter extends BaseMessageFilter {
         return [
           false,
           [
-            result.map((item, index) => `[${index+1}]"${item.body}"\nURL:${item.href}`).join('\n\n'),
-            "Instructions: Using the provided web search results, write a comprehensive reply to the given query. Make sure to cite results using [[number](URL)] notation after the reference. If the provided search results refer to multiple subjects with the same name, write separate answers for each subject.",
+            "Web search results:\n"
+            result.map((item, index) => `[${index+1}] "${item.body}"\nURL:${item.href}`).join('\n\n'),
+            "\n\n Instructions: Using the provided web search results, write a comprehensive reply to the given query. Make sure to cite results using [[number](URL)] notation after the reference. If the provided search results refer to multiple subjects with the same name, write separate answers for each subject.",
             `Query: ${resultMessage}`,
             "Reply in 中文"
           ].join('\n')
