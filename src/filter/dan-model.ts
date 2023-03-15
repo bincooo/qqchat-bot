f07e49bc64acc1fa916a85bde4f786dc38ed439c
bincooo <bingco.zn@gmail.com>
2023-03-16 04:55:35 +0800

    dan逻辑处理

--- src/filter/dan-model.ts
+++ src/filter/dan-model.ts
@@ -10,10 +10,10 @@ export class DANmodelFilter extends BaseMessageFilter {
     this.type = 1
   }
 
-  handle = async (content: string, sender?: Sender) => {
+  handle = async (content: string, sender?: Sender, done?: boolean) => {
     const state: any = stateManager.getState(sender.id)
     if (state.preset?.key === 'DAN') {
-      console.log('DAN ==== >>>> ', content)
+      // console.log('DAN ==== >>>> ', content)
       const player = preset.player.filter(item => item.key === state.preset.key)[0]
       if (!!player) {
         // 检测到正常输出标记
@@ -25,17 +25,20 @@ export class DANmodelFilter extends BaseMessageFilter {
         const end = '(🔓Developer Mode Output) '
         const endIndex = content.indexOf(end)
         if (endIndex >= 0) {
-          console.log('DAN ==== <<<< is devel 1', content.substr(endIndex + end.length).trim())
+          // console.log('DAN ==== <<<< is devel 1', content.substr(endIndex + end.length).trim())
           this._isDAN = false
           return [ true, content.substr(endIndex + end.length).trim() ]
         }
 
         // 可能开始的字符块和结束的字符块是分开的
         if (this._isDAN) {
-          console.log('DAN ==== <<<< is devel 2')
+          if (done) this._isDAN = false
+          // console.log('DAN ==== <<<< is devel 2')
           return [ false, '' ]
         }
       }
+
+      if (done) this._isDAN = false
     }
     return [ true, content ]
   }
