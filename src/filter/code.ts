1cda2a5a63ea55783ade9318e691ea41106fde58
bincooo <bingco.zn@gmail.com>
2023-03-24 10:44:35 +0800

    md模式下添加搜图功能

--- src/filter/code.ts
+++ src/filter/code.ts
@@ -33,8 +33,8 @@ export class CodeFilter extends BaseMessageFilter {
         ]
       .join('\n')
 
+      console.log("CodeFilter ===> search images", content, content.match(/来[0-9]?张[^(图片)]?图片/g))
       if (content.match(/来[0-9]?张[^(图片)]?图片/g)) {
-        console.log("CodeFilter ===> search images", content)
         const result: QueueReply = async (reply) => {
           await reply('从现在起, 当你想发送一张照片时，请使用 Markdown ,并且 不要有反斜线, 不要用代码块。使用 Unsplash API (https://source.unsplash.com/1280x720/? < PUT YOUR QUERY HERE >)。如果你明白了，请回复“明白”')
           return resultMessage.trim()
