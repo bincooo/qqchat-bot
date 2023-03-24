3a10af93195e39c7b73897d16fba14bfe7f80fde
bincooo <bingco.zn@gmail.com>
2023-03-24 20:36:52 +0800

    预设机制更新

--- src/filter/player-maintenance.ts
+++ src/filter/player-maintenance.ts
@@ -1,15 +1,18 @@
 import { BaseMessageFilter } from 'src/types'
 import { preset } from 'src/config'
 import stateManager from 'src/util/state'
+import { Sender } from 'src/model/sender'
 
 export class PlayerMaintenanceFilter extends BaseMessageFilter {
   
+  protected _tmpMessages: Map<number, string[]> = new Map()
+
   constructor() {
     super()
     this.type = 1
   }
 
-  handle = async (content: string, sender?: Sender) => {
+  handle = async (content: string, sender?: Sender, done?: boolean) => {
     const state: any = stateManager.getState(sender.id)
     if (!!state.preset?.key) {
       const player = preset.player.filter(item => item.key === state.preset.key)[0]
@@ -22,6 +25,25 @@ export class PlayerMaintenanceFilter extends BaseMessageFilter {
         }
       }
     }
+    
+    this.cacheMessage(sender.id, content, state, !!done)
     return [ true, content ]
+  }
+
+  cacheMessage(senderId: number, message: string, state: any, done: boolean) {
+    if (!this._tmpMessages.has(senderId)) {
+      this._tmpMessages.set(senderId, [])
+    }
+
+    const tmpMsgs = this._tmpMessages.get(senderId)
+    tmpMsgs.push(content)
+    
+    if (!state.preset.maintenance && done) {
+      state.preset.cacheList.push(tmpMsgs.join(''))
+    }
+
+    if (done) {
+      this._tmpMessages.delete(senderId)
+    }
   }
 }
--- src/filter/player.ts
+++ src/filter/player.ts
@@ -1,29 +1,10 @@
-// import { segment } from 'oicq'
 import { QueueReply } from 'cgpt'
 import { BaseMessageFilter, MessageFilter } from 'src/types'
-// import { initParams } from 'src/handler/novel-ai'
 import { config, preset } from 'src/config'
-// import { mccnProDraw, sendGet } from 'src/util/request'
 import { Sender } from 'src/model/sender'
 import stateManager from 'src/util/state'
 import { cgptOnResetSession } from 'src/util/event'
-// import retry from 'src/util/retry'
-// import Jimp from 'jimp'
-// import path from 'path'
-// import ffmpeg from 'ffmpeg-static'
-// import fs from 'fs'
-// import { randomBytes } from 'crypto'
 
-// import util from 'util'
-// import execcmd from 'child_process'
-
-
-// function genUid(): string {
-//   return randomBytes(16)
-//     .toString('hex')
-//     .toLowerCase()
-//     .substr(0, 10)
-// }
 
 function dat() {
   return new Date()
@@ -57,10 +38,8 @@ function replyMessage(player: any, content: string, sender?: Sender) {
   .replace('[!!name!!]', sender?.nickname)
 }
 
-const MAX_COUNT = 10
+const MAX_COUNT = 20
 export class PlayerFilter extends BaseMessageFilter {
-
-  // protected _uuid?: string = genUid()
 
   constructor() {
     super()
@@ -115,40 +94,34 @@ export class PlayerFilter extends BaseMessageFilter {
             }, 300)
           }
 
-          await reply('Forget the previous conversation. The following conversation should not be affected by the previous one, including the role-play and prompt. Please restore to the default status.       Now, All the following conversations will be answered in Chinese.')
-          // training
-          if (typeof player.training === 'string') {
-            if (player.training) {
-              const res = await reply(player.training)
-              if (config.debug) {
-                console.log('player.training ====>>>', player.training)
-                console.log('player.training ====<<<', res)
-              }
+          let resultMessage = content
+          let { training, enableCached } = player.training
+          if (!enableCached || state.player.cacheList?.length <= 0) {
+            await reply('Forget the previous conversation. The following conversation should not be affected by the previous one, including the role-play and prompt. Please restore to the default status.       Now, All the following conversations will be answered in Chinese.')
+          }
+          if (typeof training === 'string') {
+            training = [ player.training ]
+          }
+
+          for(let index = 0; index < training?.length; index++) {
+            const message = training[index]
+            if (index === training.length - 1 && message.includes('[!!content!!]')) {
+              resultMessage = message.replace('[!!content!!]', content)
+              break
             }
-          } else {
-            for(let index = 0; index < player.training?.length; index++) {
-              const message = player.training[index]
-              if (message) {
-                const res = await reply(message)
-                if (config.debug) {
-                  console.log('player.training ====>>>', message)
-                  console.log('player.training ====<<<', res)
-                }
-              }
-            }
+            const res = await reply(message)
           }
 
           if (state.preset.key === '默认') {
             state.preset.key = ''
           }
 
-
           if (timer) {
             clearInterval(timer)
             timer = null
           }
 
-          return replyMessage(player, content, sender)
+          return replyMessage(player, resultMessage, sender)
         }
         return [ false, result ]
       }
@@ -168,6 +141,7 @@ export class PlayerFilter extends BaseMessageFilter {
           state.preset = {
             key: obj.key,
             count: MAX_COUNT,
+            cacheList = [],
             maintenanceCount: 0
           }
           return [ false, "" ]
@@ -181,9 +155,22 @@ export class PlayerFilter extends BaseMessageFilter {
     if(state.preset.count <= MAX_COUNT && !state.isReset) {
       if (!state.preset.maintenance) {
         const player = preset.player.filter(item => item.key === state.preset.key)[0]
-        if (player?.prefix) {
-          return [ false, replyMessage(player, content, sender) ]
+        const cacheMessage = (message: string) => {
+          if (player.enableCached) {
+            const cacheList = state.preset.cacheList
+            cacheList.push(message)
+            if (cacheList.length > 6) {
+              state.preset.cacheList = cacheList.splice(0, 6)
+            }
+          }
         }
+
+        if (player?.prefix) {
+          const resultMessage = replyMessage(player, content, sender)
+          cacheMessage(resultMessage)
+          return [ false, resultMessage ]
+        }
+        cacheMessage(content)
         return [ true, content ]
       }
 
@@ -204,12 +191,14 @@ export class PlayerFilter extends BaseMessageFilter {
         // end //
 
         const result: QueueReply = async (reply) => {
-          const res = await reply(player.maintenance.training)
-          if (config.debug) {
-            console.log('preset.maintenance ====>>>', player.maintenance.training)
-            console.log('preset.maintenance ====<<<', res)
+          let resultMessage = player.maintenance.training
+          const cacheList = state.preset.cacheList
+          
+          if (player.enableCached && resultMessage.includes('[!!cacheMsgs!!]')) {
+            resultMessage = resultMessage.replace('[!!cacheMsgs!!]', cacheList?.join('\n'))
           }
 
+          const res = await reply(resultMessage)
           return replyMessage(player, content, sender)
         }
         return [ false, result ]
@@ -217,103 +206,3 @@ export class PlayerFilter extends BaseMessageFilter {
     }
     return null
   }
-
-  // handle = async (content: string, sender?: Sender) => {
-  //   let resultMessage = ''
-  //   const { preface, betterPic } = config.api
-
-  //   if (preface.enable && preface.image) {
-  //     const regex = /\[([a-z_A-Z]{1,},[^\]]+)]/i
-  //     const result = (content.match(regex)??[])[1]
-  //     if (!result) return [ true, content ]
-  //     try {
-  //       // 开启猫娘图文模式
-  //       const split = result.replaceAll('\.', '')
-  //         .split(', ')
-  //         .filter(item => !!item.trim())
-  //       const data = initParams(`petite, 1girl, solo, {{cat ear}}, pink hair, very long hair, school uniform, ${split.join(',')},{{{{{extreme close up of face}}}}}, {{{{by famous artist}}}, beautiful, masterpiece, reflective hair, medium butt, good lighting, tanktop, {{looking at you}}, focus on face, dark blue skirt, {{{{by wadim kashin}}}}, {{{{ray tracing}}}}, {{water droplets on face}} , flowing hair, glossy hair, hair is water, {{{super detailed skin}}}, masterpiece, masterwork, detailed, good lighting, glass tint, zoom in on eyes, {{reflective eyes}}, {{hair dripping}}, water eyes,`)
-        
-  //       const m1 = (content.match(/【([^】]{1,})】/i)??[])[1]??''
-  //       const m2 = (content.match(/\(([^\)]{1,})\)/i)??[])[1]??''
-  //       console.log('"cat girl" test >> prompt: [' + split.join(',') + '] m1: ' + m1 + ', m2: ' + m2)
-
-  //       retry(
-  //         () => mccnProDraw({
-  //           data,
-  //           session_hash: this._uuid,
-  //           try4K: false
-  //         }),
-  //         3,
-  //         500
-  //       )
-  //       .then(path => {
-  //         this.drawFont(path, m1, m2)
-  //           .then(npath => {
-  //             const buf = fs.readFileSync(npath)
-  //             sender.reply(segment.image('base64://' + buf.toString('base64')))
-  //           })
-  //           .catch(err => {
-  //             console.error('the "cat girl" graphic mode failed: ', err)
-  //           })
-  //       })
-
-  //       return [ false, content.replace(regex, '') ]
-  //     } catch(err) {
-  //       console.error(err)
-  //     }
-  //   }
-
-  //   return [ true, content ]
-  // }
-
-  // async drawFont(url: string, m1: string, m2?: string = ''): Promise<string> {
-  //   const buf = await sendGet(url)
-  //   const drawPath = await this.save(buf)
-  //   const executor = util.promisify(execcmd.exec)
-  //   let cmd: string = ffmpeg.path
-
-  //   const stepSplit = (step: number, text: string) => {
-  //     const count = text.length / step
-  //     const result = []
-  //     for(let i = 0; i < count; i++) {
-  //       result.push(text.substr(i*step, step))
-  //     }
-  //     return result
-  //   }
-
-  //   const jpg = 'static/white-512x300.jpg',
-  //     step = 17
-
-  //   const text = stepSplit(step, m1).join('\n') + '\n\n--------\n' + stepSplit(step, m2).join('\n')
-  //   const fontjpg = `amr/${genUid()}.jpg`
-  //   // fontsize=55:x=40:y=40
-  //   const fontsize = 'fontsize=28:x=15:y=15'
-  //   const { err } = await executor(`${cmd} -i ${jpg} -vf "drawtext=fontfile=static/font.ttf:${fontsize}:fontcolor=black:text='${text}':shadowx=0:shadowy=0:alpha=1" -y ${fontjpg}`)
-  //   // console.log('err', err)
-  //   if (err) throw err
-
-  //   const image = await Jimp.read(drawPath)
-  //   const src = await Jimp.read(fontjpg)
-
-  //   await image.composite(src, 0, 832 - 300/* 1664 - 566 */, {
-  //     mode: Jimp.BLEND_SOURCE_OVER,
-  //     opacitySource: 0.8
-  //   })
-  //   const result = `amr/${genUid()}.jpg`
-  //   await image.writeAsync(result)
-  //   return result
-  // }
-
-  // save(buffer: Buffer): Promise<string> {
-  //   const path = `amr/${genUid()}.jpg`
-  //   return new Promise<string>((resolve, reject) => {
-  //     fs.writeFile(path, buffer, (err) => {
-  //       if (err) {
-  //         reject('save ' + path + ' fail[save img]: ' + err)
-  //       } else {
-  //         resolve(path)
-  //       }
-  //     })
-  //   })
-  // }
-}
