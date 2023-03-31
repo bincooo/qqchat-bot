99888846eb454f5e62c485802e9e276a3565e530
bincooo <bingco.zn@gmail.com>
2023-03-31 22:03:31 +0800

    提交代码

--- src/filter/player.ts
+++ src/filter/player.ts
@@ -36,7 +36,7 @@ function datFmt() {
   return `${y}-${fmt(m)}-${fmt(d)} ${fmt(h)}:${fmt(mm)}:${fmt(s)}`
 }
 
-function replyMessage(prefix: string, content: string, sender?: Sender) {
+function replyMessage(prefix: string = "", content: string, sender?: Sender) {
   // emoji 过滤
   const regex = /[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/g
   let nickname = sender.nickname
